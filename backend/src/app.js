b595c1553b686e7ca1b834a81125822a10558f62
surajmendhe5573 <surajmendhe470@gmail.com>
2025-12-04 09:13:14 +0530

    updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,9 @@ const app = express();
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(compression());
 app.use(express.static("public")); 
 
